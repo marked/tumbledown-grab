68984c20a53247f4aee2232f06c7d770acd5f92c
kiska <qingbest@fastmail.fm>
2018-12-06 20:51:38 +1100

    Do not grab facebook login

--- tumblr.lua
+++ tumblr.lua
@@ -37,7 +37,7 @@ end
 allowed = function(url, parenturl)
   local blog = read_file("blog")
   local concat = "http://" .. blog .. ".tumblr.com"
-  if string.match(url, "'+") or string.match(url, "[<>\\%*%$;%^%[%],%(%)]") or string.match(url, "//$") or string.match(url, "https?://www") then
+  if string.match(url, "'+") or string.match(url, "[<>\\%*%$;%^%[%],%(%)]") or string.match(url, "//$") or string.match(url, "https?://www") or string.match(url, "https?://m.facebook.com/login.php") then
     return false
   end
   
