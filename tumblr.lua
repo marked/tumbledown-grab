a31d866929619b261d501c43cfa5a39a7d206067
kiska <qingbest@fastmail.fm>
2018-12-06 21:00:10 +1100

    Attempt 3: Removing facebook and reddit login pages

--- tumblr.lua
+++ tumblr.lua
@@ -37,7 +37,7 @@ end
 allowed = function(url, parenturl)
   local blog = read_file("blog")
   local concat = "http://" .. blog .. ".tumblr.com"
-  if string.match(url, "'+") or string.match(url, "[<>\\%*%$;%^%[%],%(%)]") or string.match(url, "//$") or string.match(url, "https?://www") or string.match(url, "https?://m.facebook.com/login.php") or string.match(url, "px.srvcs.tumblr.com") or string.match(url, "https?://www.reddit.com/login") then
+  if string.match(url, "'+") or string.match(url, "[<>\\%*%$;%^%[%],%(%)]") or string.match(url, "//$") or string.match(url, "https?://www") or string.match(url, "https?://.+.facebook.com/login.php") or string.match(url, "px.srvcs.tumblr.com") or string.match(url, "https?://.+.reddit.com/login") then
     return false
   end
   
